0b8e96e7c19e6e60f9c55b2eea5641c451770fe0
derekoko <derek.koh@g.austincc.edu>
2021-02-09 19:10:25 -0600

    Updated notes

--- src/examples/03_module/01_if/if.cpp
+++ src/examples/03_module/01_if/if.cpp
@@ -1,8 +1,26 @@
 //write include statement for if header
-
+#include "if.h"
 
 
 //write code for the is_even function that returns true if num is even, false otherwise
+bool is_even(int num)
+{
+    auto even = false;
+
+    if(num % 2 == 0) //modulus returns the remainder seeing if its even or odd.
+    {
+        even = true;
+    }
+
+    return even;
+}
+
+/*can also do:
+    bool is_even(int_num)
+    {
+        return num % 2 == 0
+    }
+*/
 
 
 //write code for function named overtime that accepts a double hours and returns true if 
